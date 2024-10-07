3458cce5f59144c4d12cbc08f23891411b23d655
sainudheenp <sainudheen2003@gmail.com>
2024-10-07 21:57:21 +0530

    fetched wpm

--- utils/typing_data.py
+++ utils/typing_data.py
@@ -5,7 +5,12 @@ from selenium.webdriver.firefox.service import Service as Service
 from selenium.webdriver.firefox.options import Options
 import time
 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
+
+from dotenv import load_dotenv
+import os
 
 
 # incognito
@@ -13,16 +18,10 @@ import time
 options = Options()
 options.add_argument('-private')
 options.add_argument('--no-sandbox')
+options.add_argument("--headless")
 
 
 
-
-
-# driver path
-# geckodriver_path = './driver/geckodriver'
-# service =FirefoxService(geckodriver_path)
-
-# driver = webdriver.Firefox(service=service, options=options)  
 
 
 service = Service('./driver/geckodriver')
@@ -32,21 +31,41 @@ driver = webdriver.Firefox(service=service)
 
 # function for logging into monkeytyping.com
 
-driver.get("https://monkeytype.com/account")
+driver.get("https://monkeytype.com/login")
 
 time.sleep(10)
+
+# Cache_btn = driver.find_elements_by_class_name("acceptAll").click()
+print("hi1")
+
+try:
+    dd = driver.find_element(By.CLASS_NAME, "acceptAll").click()
+    print("Cookie popup accepted.")
+except Exception as e:
+    print("No cookie button found or other error:", e)
+
+
+
+
 
 
 # get fields
 userName_field =driver.find_element(By.NAME, "current-email")
 password_field =driver.find_element(By.NAME, "current-password")
-signIn_btn = driver.find_element(By.CLASS , "signIn")
+signIn_btn = driver.find_element(By.CLASS_NAME, "signIn")
 
 
 #entering inot fields
-userName_field.send_keys("sainudheenzain313@gmail.com")
-password_field.send_keys("")
+userName_field.send_keys("email")
+password_field.send_keys("psd")
 
 signIn_btn.click()
 
+time.sleep(10)
 
+
+wpm_value = driver.find_element(By.CSS_SELECTOR, ".averageWpm .val")
+ssf =wpm_value.text
+print(f"avg wpm : {ssf}")
+
+driver.quit()
