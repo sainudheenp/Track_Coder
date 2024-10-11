3f4182ce1d703d3884edaf1beafa21080609c21d
sainudheenp <sainudheen2003@gmail.com>
2024-10-11 18:09:46 +0530

    sample loc

--- Gchrome/utils/lines_of_code.py
+++ Gchrome/utils/lines_of_code.py
@@ -4,6 +4,9 @@ import datetime
 
 file_path = os.path.expanduser("~/TrackMe/daily-progress-tracker.json")
 
+html = 0
+css = 0
+js =0
 
 with open(file_path) as json_file:
     json_data = json.load(json_file)
@@ -12,5 +15,13 @@ with open(file_path) as json_file:
     last_lines = json_data[last_day]
 
 
+    html = last_lines["lines_of_code"]["html"]
+    css = last_lines["lines_of_code"]["css"]
+    # js = last_lines["lines_of_code"]["js"]
+    total = last_lines["lines_of_code"]["total"]
+   
+    print(f"HTML    : {html}")
+    print(f"CSS     : {css}")
+    print(f"JS      : {js}")
+    print(f"TOTAL   : {total}")
 
-    print(last_lines)
