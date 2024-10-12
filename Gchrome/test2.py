55a8ef46af3ccbfba565951dfcb2d51e949db803
sainudheenp <sainudheen2003@gmail.com>
2024-10-12 19:49:07 +0530

    safe point

--- Gchrome/test2.py
+++ Gchrome/test2.py
@@ -4,23 +4,22 @@ import datetime
 
 date = datetime.date.today()
 
-# Corrected file extension
 Xl_path = os.path.expanduser("~/TrackCoder/trackcoder.xlsx")
 
-if os.path.exists(Xl_path):
-    workbook = openpyxl.load_workbook(Xl_path)  # Load existing workbook
-    sheet = workbook.active  # Select the active sheet
-else:
-    workbook = openpyxl.Workbook()  # Create a new workbook
-    sheet = workbook.active  # Now the sheet variable will not be None
-    sheet.append(["Date", "Focus", "Wpm", "Code Time", "Active Code Time"])  # Append headers
+os.makedirs(os.path.dirname(Xl_path), exist_ok=True)
 
+if os.path.exists(Xl_path):
+    workbook = openpyxl.load_workbook(Xl_path)
+    sheet = workbook.active
+else:
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.append(["Date", "Focus", "Wpm", "Code Time", "Active Code Time"])
 new_data = [
     [date, "1:30", "29", "7:30", "3:30"]
 ]
 
 for row in new_data:
-    sheet.append(row)  # Append new data
-
-workbook.save(Xl_path)  # Save the workbook
+    sheet.append(row)
+workbook.save(Xl_path)  
 print("Data added successfully!")
--- Gchrome/utils/xl_rw.py
+++ Gchrome/utils/xl_rw.py
@@ -4,24 +4,23 @@ import datetime
 
 date = datetime.date.today()
 
-# Corrected file extension
 Xl_path = os.path.expanduser("~/TrackCoder/trackcoder.xlsx")
+
+os.makedirs(os.path.dirname(Xl_path), exist_ok=True)
 
 if os.path.exists(Xl_path):
     workbook = openpyxl.load_workbook(Xl_path)
     sheet = workbook.active
 else:
-    open(Xl_path, 'a').close()
-    workbook = openpyxl.Workbook(Xl_path)
+    workbook = openpyxl.Workbook()
     sheet = workbook.active
-    sheet.append(["Date", "Focus", "Wpm", "Code Time", "Active Code Time"])
-
+    sheet.append(["Date", "Focus", "Wpm", "CT", "ACT"])
 new_data = [
     [date, "1:30", "29", "7:30", "3:30"]
 ]
+# sheet.column_dimensions['A'].width = 15
 
 for row in new_data:
-    sheet.append(row)  # Append new data
-
+    sheet.append(row)
 workbook.save(Xl_path)
 print("Data added successfully!")
