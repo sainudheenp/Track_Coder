35e0fb6ec2001198f602d147f2902fb0abc9aab6
sainudheenp <sainudheen2003@gmail.com>
2024-10-17 09:49:40 +0530

    some changes

--- Gchrome/utils/print_values.py
+++ Gchrome/utils/print_values.py
@@ -1,9 +1,12 @@
-def Print_values(Wpm, Focus, ACT, CT, html, css, js, total,html_total ,css_total, js_total, CT_TOTAL, ACT_TOTAL,Focus_TOTAL , T_TOTAL):
+def Print_values(Wpm, Focus, ACT, CT, html, css, js, total,html_total ,css_total, js_total, CT_TOTAL, ACT_TOTAL,Focus_TOTAL ):
+   
+    print("\n \n \n \n")
+   
     print(f"Typing  : [{Wpm}][50%]")
     print(f"Focus   : [{Focus}][{Focus_TOTAL}]")
-    print(f"ACT     : [{ACT}][{ACT_TOTAL}]")
     print(f"CT      : [{CT}][{CT_TOTAL}]")
+    print(f"ACT     : [{ACT}][{ACT_TOTAL}]")
     print(f"HTML    : [{html}][{html_total}]")
     print(f"CSS     : [{css}]{css_total}")
     print(f"JS      : [{js}][{js_total}]")
-    print(f"TOTAL   : [{total}][{T_TOTAL}]")
+    print(f"TOTAL   : [{total}][{"0"}]")
