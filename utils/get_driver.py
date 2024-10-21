fe8ce0e60e57ce0003b1ef85edac1641843f2934
sainudheenp <sainudheen2003@gmail.com>
2024-10-21 13:49:49 +0530

    time sleep added  3s ,typing

--- utils/get_driver.py
+++ utils/get_driver.py
@@ -8,7 +8,7 @@ def get_driver():
     options.add_argument('--incognito')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
 
     # Pass the options here
     print("Driver installing...")
--- utils/print_values.py
+++ utils/print_values.py
@@ -7,6 +7,6 @@ def Print_values(Wpm, Focus, ACT, CT, html, css, js, total,html_total ,css_total
     print(f"CT      : [{CT}][{CT_TOTAL}]")
     print(f"ACT     : [{ACT}][{ACT_TOTAL}]")
     print(f"HTML    : [{html}][{html_total}]")
-    print(f"CSS     : [{css}]{css_total}")
+    print(f"CSS     : [{css}]{css_total}]")
     print(f"JS      : [{js}][{js_total}]")
     print(f"TOTAL   : [{total}][{T_Total}]")
